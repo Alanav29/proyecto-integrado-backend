10b94038fb0403934860e02f3e0df133bf2d446d
Alanav29 <josephalannavarro@gmail.com>
2024-01-18 11:49:00 -0600

    fix: product updating

--- sp3_ecommerce/src/main/java/org/generation/app/service/impl/ProductServiceImpl.java
+++ sp3_ecommerce/src/main/java/org/generation/app/service/impl/ProductServiceImpl.java
@@ -74,13 +74,35 @@ public class ProductServiceImpl implements ProductService {
 	@Override // Se puede mejorar creo¿
 	public Product updateProduct(Product product, Long id) {
 		Product existingProduct = getProductById(id);
-		existingProduct.setTitle( product.getTitle() );
-		existingProduct.setPrice( product.getPrice() );
-		existingProduct.setTechnique( product.getTechnique() );
-		existingProduct.setWidth( product.getWidth() );
-		existingProduct.setHeight( product.getHeight() );
-		existingProduct.setColor( product.getColor() );
-		existingProduct.setImg( product.getImg() );
+		
+		if(!product.getTitle().equals(""))existingProduct.setTitle( product.getTitle() );
+		if(product.getPrice() != 0)existingProduct.setPrice( product.getPrice() );
+		if(!product.getTechnique().equals(""))existingProduct.setTechnique( product.getTechnique() );
+		if(product.getWidth() != 0)existingProduct.setWidth( product.getWidth() );
+		if(product.getHeight() != 0)existingProduct.setHeight( product.getHeight() );
+		if(!product.getColor().equals(""))existingProduct.setColor( product.getColor() );
+		if(!product.getImg().equals("")) {
+			
+			Cloudinary cloudinary = new Cloudinary("cloudinary://992384659953544:Ff7CsjP8fpYGNDPRWPnEGamkal4@dtyazhppg");
+			try {
+				String base64Image = product.getImg();
+				int commaIndex = base64Image.indexOf(",");
+				String base64Data = base64Image.substring(commaIndex + 1);
+				byte[] imageBytes = Base64.getDecoder().decode(base64Data);
+	
+	            // Subir la imagen a Cloudinary
+	            Map uploadResult = cloudinary.uploader().upload(imageBytes, ObjectUtils.emptyMap());
+	            String secureUrl = (String) uploadResult.get("secure_url");
+	            existingProduct.setImg(secureUrl);
+	            
+	            
+	            
+	        } catch (Exception e) {
+	        	
+	        	throw new IllegalStateException("Cannot upload img " );
+	        }
+			
+		}
 		return productRepository.save( existingProduct );
 	}
 	
